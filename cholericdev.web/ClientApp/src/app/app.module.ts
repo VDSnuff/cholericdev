19e01f061915385d8e89611ab08f462cf996b47c
Viacheslav <v.dovnich@gmail.com>
2019-07-31 23:52:50 +0200

    Cumulative update

--- cholericdev.web/ClientApp/src/app/app.module.ts
+++ cholericdev.web/ClientApp/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { DataService } from './shared/dataService';
+import { ContactComponent } from './contact/contact.component'
+import { AboutMeComponent } from './about-me/about-me.component'
+import { ProjectListComponent } from './project-list/project-list.component'
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { DataService } from './shared/dataService';
     NavMenuComponent,
     CounterComponent,
     FetchDataComponent,
-    PostListComponent
+    PostListComponent,
+    ContactComponent,
+    AboutMeComponent,
+    ProjectListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +32,10 @@ import { DataService } from './shared/dataService';
     RouterModule.forRoot([
       { path: '', component: PostListComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent }
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'about-me', component: AboutMeComponent },
+      { path: 'project-list', component: AboutMeComponent }
     ])
   ],
   providers: [
